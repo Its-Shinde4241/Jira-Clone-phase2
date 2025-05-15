a6e575959041cd0cb4bddfbddeca945180a17c5f
shubhamshinde5356 <shindeshubham5356@gmail.com>
2025-05-15 11:17:58 +0530

    solving env problems

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -3,8 +3,8 @@ import { configDotenv } from 'dotenv';
 
 configDotenv();
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.SUPABASE_URL || 'https://kkekbmtmjmdcngvxfedr.supabase.co';
+const supabaseKey = process.env.SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtrZWtibXRtam1kY25ndnhmZWRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0MjczMTYsImV4cCI6MjA2MjAwMzMxNn0.1fwxU50jQgtabv_ACey4ATc5rbLer0dIGIPMLFoilQA';
 if (!supabaseKey) {
   console.log('NO KEY FOUND');
 }
